304552db5af0c9bb25b558f20b5e0e0968dc003e
Ilham Fadhilah <ilhamfi_2701@yahoo.com>
2019-11-30 09:32:07 +0700

    Use Leave One Out Cross Validation

--- core_1svr.py
+++ core_1svr.py
@@ -5,6 +5,7 @@ from sklearn.svm import SVR
 from sklearn.preprocessing import MinMaxScaler
 from skfeature.function.statistical_based import f_score
 from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
 
 def main():
     sc = MinMaxScaler(feature_range=(0,10))
@@ -40,21 +41,32 @@ def main():
 
     # 5. predict
     regressor = SVR(gamma='scale', C=1.0, epsilon=0.2)
-
     selected_feature = np.array(selected_feature)
-    X_train = selected_feature[0:58,1:]
-    y_train = y[0:58]
+    y_pred = []
 
-    regressor.fit(X_train, y_train)
+    for train_sequence in range(len(selected_feature)):
+        X_train = []
+        y_train = []
+        predict_row = []
+        for sequence in range(len(selected_feature)):
+            if(train_sequence == sequence):
+                predict_row.append(selected_feature[sequence,1:])
+            else:
+                X_train.append(selected_feature[sequence,1:])
+                y_train.append(y[sequence])
+        regressor.fit(X_train, y_train)
+        prediction_result = regressor.predict(predict_row)
+        y_pred.extend(prediction_result)
 
-    y_pred = regressor.predict(selected_feature[59:118, 1:])
     print(y_pred)
 
     # 6. count accuracy prediction
-    y_true = y[59:118]
+    y_true = y[0:]
     accuracy_score = r2_score(y_true, y_pred)
+    rmse_score = mean_squared_error(y_true, y_pred)
     
     print(accuracy_score)
+    print(rmse_score)
 
 def average(value):
     return sum(value) / len(value)
