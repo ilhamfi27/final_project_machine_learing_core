b6064928bdd83218bbc47edb9f83b1011ea88f20
Ilham Fadhilah <ilhamfi_2701@yahoo.com>
2019-12-07 09:57:58 +0700

    add script for reporting

--- alpha_f_score.py
+++ alpha_f_score.py
@@ -7,6 +7,7 @@ from skfeature.function.statistical_based import f_score
 from sklearn.metrics import r2_score
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import LeaveOneOut
+from sklearn.model_selection import train_test_split
 
 def main():
     sc = MinMaxScaler(feature_range=(0,10))
@@ -40,25 +41,40 @@ def main():
     regressor = SVR(gamma='scale', C=1.0, epsilon=0.2)
     X = np.array(ranked_index)
     y_pred = []
+    y_true = []
+
+    # X_train, X_test, y_train, y_test = \
+    #     train_test_split(X, y, test_size=0.5, shuffle=False)
+    # regressor.fit(X_train, y_train)
+    # y_pred = regressor.predict(X_test)
+    # y_true = y_test
 
     loo = LeaveOneOut()
     loo.get_n_splits(X)
 
     for train_index, test_index in loo.split(X):
         X_train, X_test = X[train_index], X[test_index]
-        y_train = y[train_index]
+        y_train, y_test = y[train_index], y[test_index]
         regressor.fit(X_train, y_train)
         y_pred.extend(regressor.predict(X_test))
+        y_true.extend(y_test)
 
-    print(y_pred)
+    # print(y_pred)
 
     # 6. count prediction accuracy
-    y_true = y[0:]
-    accuracy_score = r2_score(y_true, y_pred)
+    accuracy_score  = r2_score(y_true, y_pred)
     rmse_score = mean_squared_error(y_true, y_pred)
     
-    print(accuracy_score)
-    print(rmse_score)
+    print("Skor Akurasi : ", accuracy_score)
+    print("Skor RMSE : ", rmse_score)
+
+    # plotting
+    plt.scatter(y_true, y_pred)
+    plt.plot(y_true, y_true)
+    plt.title("Plot Hasil Prediksi SVR")
+    plt.xlabel("Hasil Prediksi")
+    plt.ylabel("Label Awal")
+    plt.show()
 
 def average(value):
     return sum(value) / len(value)
