c7055bcf7d7b95aa9110dd1619542a668853e963
Ilham Fadhilah <ilhamfi_2701@yahoo.com>
2020-05-13 14:16:08 +0700

    tuning and experiment

--- core/svr_experiments.py
+++ core/svr_experiments.py
@@ -33,87 +33,69 @@ def main():
 
     best_sort_feature = []
     # # 4. feature selection
-    ranked_index = f_score.f_score(X, y, mode="index")
-    print("F SCORE")
-    print(ranked_index)
+    f_score_ranked_index = f_score.f_score(X, y, mode="index")
 
-    # print("\n=============================================================\n")
-    # X_feature = X.astype(int)
-    # y_label = y.astype(int)
-    # ranked_index = chi_square.chi_square(X_feature, y_label, mode="index")
-    # print("CHI SQUARE")
-    # print(ranked_index)
-    #
-    # print("\n=============================================================\n")
-    # ranked_index = CFS.cfs(X, y)
-    # print("CFS")
-    # print(ranked_index)
+    X_feature = X.astype(int)
+    y_label = y.astype(int)
+    chi_square_ranked_index = chi_square.chi_square(X_feature, y_label, mode="index")
 
-    # SVR
-    regressor = SVR(gamma=0.0001, C=100, epsilon=0.1, kernel='rbf')
-    # get best feature predict score
-    best_pred, best_score, result, ten_column_predictions \
-        = trainf(X[:, ranked_index[:]], y, regressor=regressor)
+    cfs_ranked_index = CFS.cfs(X, y)
 
-    print("SVR")
-    for num, score_res in enumerate(result):
-        print("{}. {} column, R2_SCOREnya adalah {} dan RMSEnya adalah {}"
-              .format(num + 1, score_res[0], score_res[1], score_res[2]))
-    print("\n==============================================================\n")
+    fs_algorithms = [f_score_ranked_index, chi_square_ranked_index]
+    for ranked_index in fs_algorithms:
+        # SVR
+        regressor = SVR(gamma=0.0001, C=100, epsilon=0.1, kernel='rbf')
+        # get best feature predict score
+        best_pred, best_score, result, ten_column_predictions \
+            = trainf(X[:, ranked_index[:]], y, regressor=regressor)
 
-    # Linear Regression
-    regressor = LinearRegression()
-    # get best feature predict score
-    best_pred, best_score, result, ten_column_predictions \
-        = trainf(X[:, ranked_index[:]], y, regressor=regressor)
+        print("SVR")
+        for num, score_res in enumerate(result):
+            r2 = '{0:.5g}'.format(score_res[1])
+            rmse = '{0:.5g}'.format(score_res[2])
+            print("{}. {} column, R2_SCOREnya adalah {} dan RMSEnya adalah {}"
+                  .format(num + 1, score_res[0], r2, rmse))
+        print("BEST", '{0:.5g}'.format(best_score))
+        print("====================================================================")
 
-    print("Linear Regression")
-    for num, score_res in enumerate(result):
-        print("{}. {} column, R2_SCOREnya adalah {} dan RMSEnya adalah {}"
-              .format(num + 1, score_res[0], score_res[1], score_res[2]))
-    print("\n==============================================================\n")
+        # Linear Regression
+        regressor = LinearRegression()
+        # get best feature predict score
+        best_pred, best_score, result, ten_column_predictions \
+            = trainf(X[:, ranked_index[:]], y, regressor=regressor)
 
-    # KNN Regressor
-    regressor = KNeighborsRegressor(n_neighbors=1)
-    # get best feature predict score
-    best_pred, best_score, result, ten_column_predictions \
-        = trainf(X[:, ranked_index[:]], y, regressor=regressor)
+        print("Linear Regression")
+        for num, score_res in enumerate(result):
+            r2 = '{0:.5g}'.format(score_res[1])
+            rmse = '{0:.5g}'.format(score_res[2])
+            print("{}. {} column, R2_SCOREnya adalah {} dan RMSEnya adalah {}"
+                  .format(num + 1, score_res[0], r2, rmse))
+        print("BEST", '{0:.5g}'.format(best_score))
+        print("====================================================================")
 
-    print("KNN Regressor")
-    for num, score_res in enumerate(result):
-        print("{}. {} column, R2_SCOREnya adalah {} dan RMSEnya adalah {}"
-              .format(num + 1, score_res[0], score_res[1], score_res[2]))
-    print("\n==============================================================\n")
+        # KNN Regressor
+        regressor = KNeighborsRegressor(n_neighbors=1)
+        # get best feature predict score
+        best_pred, best_score, result, ten_column_predictions \
+            = trainf(X[:, ranked_index[:]], y, regressor=regressor)
 
-    # predict
-    # regressor = SVR(gamma=0.0001, C=100, epsilon=0.1, kernel='rbf')
-    # y_pred = []
-    # y_true = []
-    #
-    # loo = LeaveOneOut()
-    # loo.get_n_splits(X)
-    #
-    # si_X = best_features[:, :96]
-    # X_selected = si_X
-    # loo = LeaveOneOut()
-    # loo.get_n_splits(si_X)
-    #
-    # for train_index, test_index in loo.split(X_selected):
-    #     X_train, X_test = X_selected[train_index], X_selected[test_index]
-    #     y_train, y_test = y[train_index], y[test_index]
-    #
-    #     regressor.fit(X_train, y_train)
-    #     y_pred.extend(regressor.predict(X_test))
-    #     y_true.extend(y_test)
-    #
-    # print(r2_score(y_true, y_pred))
+        print("KNN Regressor")
+        for num, score_res in enumerate(result):
+            r2 = '{0:.5g}'.format(score_res[1])
+            rmse = '{0:.5g}'.format(score_res[2])
+            print("{}. {} column, R2_SCOREnya adalah {} dan RMSEnya adalah {}"
+                  .format(num + 1, score_res[0], r2, rmse))
+        print("BEST", '{0:.5g}'.format(best_score))
+        print("====================================================================")
+        print("====================================================================\n\n\n")
+
 
 def trainf(X, y, regressor):
     repeat = 0
     X = np.array(X)
     X_column = X.shape[1]
     result = []
-    best_score = 0
+    best_score = -999999
     best_pred = []
     ten_column_predictions = []
 
--- core/tuning_experiment.py
+++ core/tuning_experiment.py
@@ -46,54 +46,78 @@ def main():
 
     # ranked_index = CFS.cfs(X, y)
 
-    for row in X:
-        row_array = []
-        for num, feature_idx in enumerate(ranked_index):
-            row_array.append(row[feature_idx])
-        best_sort_feature.append(row_array)
+    si_X = X[:, ranked_index[:]]
 
-    best_features = np.array(best_sort_feature)
+    kernel = ['rbf', 'linear']
+    gamma = [1e-3, 1e-4]
+    C = [1, 10, 100, 1000]
+    epsilon = [0.1, 0.5, 1.0, 1.5, 2.0]
 
-    si_X = best_features[:, :96]
-    X_train, X_test, y_train, y_test = train_test_split(si_X, y, test_size=0.25)
+    loo = LeaveOneOut()
+    loo.get_n_splits(si_X)
 
-    # Set the parameters by cross-validation
-    tuned_parameters = [
-        {
-            'kernel': ['rbf'],
-            'gamma': [1e-3, 1e-4],
-            'C': [1, 10, 100, 1000],
-            'epsilon': [0.1, 0.5, 1.0, 1.5, 2.0]
-        },
-        {
-            'kernel': ['linear'],
-            'C': [1, 10, 100, 1000],
-            'epsilon': [0.1, 0.5, 1.0, 1.5, 2.0]
-        }
-    ]
+    total_search = (len(kernel) * len(gamma) * len(C) * len(epsilon)) - (len(C) * len(epsilon))
+    have_search = 0
+    best_search = []
+    best_score = 0
+    for k in kernel:
+        for c in C:
+            for e in epsilon:
+                if k == 'rbf':
+                    for g in gamma:
+                        regressor = SVR(gamma=g, C=c, epsilon=e, kernel=k)
 
+                        y_pred = []
+                        y_true = []
 
-    clf = GridSearchCV(
-        SVR(), tuned_parameters, cv = 3
-    )
-    clf.fit(X_train, y_train)
-    y_true, y_pred = y_test, clf.predict(X_test)
+                        for train_index, test_index in loo.split(si_X):
+                            X_train, X_test = si_X[train_index], si_X[test_index]
+                            y_train, y_test = y[train_index], y[test_index]
+                            regressor.fit(X_train, y_train)
+                            y_pred.extend(regressor.predict(X_test))
+                            y_true.extend(y_test)
 
-    # Results from Grid Search
-    print("\n========================================================")
-    print(" Results from Grid Search ")
-    print("========================================================")
+                        score = r2_score(y_true, y_pred)
 
-    print("\n The best estimator across ALL searched params:\n",
-          clf.best_estimator_)
+                        if best_score < score:
+                            best_score = score
+                            best_search = [regressor, "gamma {} | c {} | epsilon {} | kernel {}".format(g, c, e, k)]
 
-    print("\n The best score across ALL searched params:\n",
-          clf.best_score_)
+                        have_search += 1
+                        print("=======================================================================================")
+                        print("score", score)
+                        print("gamma {} | c {} | epsilon {} | kernel {}".format(g, c, e, k))
+                        print("progress", have_search, "out of", total_search)
+                        print("=======================================================================================")
+                else:
+                    regressor = SVR(C=c, epsilon=e, kernel=k)
 
-    print("\n The best parameters across ALL searched params:\n",
-          clf.best_params_)
-    print("\nR2 Value:\n")
-    print(r2_score(y_true, y_pred))
+                    y_pred = []
+                    y_true = []
+
+                    for train_index, test_index in loo.split(si_X):
+                        X_train, X_test = si_X[train_index], si_X[test_index]
+                        y_train, y_test = y[train_index], y[test_index]
+                        regressor.fit(X_train, y_train)
+                        y_pred.extend(regressor.predict(X_test))
+                        y_true.extend(y_test)
+
+                    score = r2_score(y_true, y_pred)
+
+                    if best_score < score:
+                        best_score = score
+                        best_search = [regressor, "c {} | epsilon {} | kernel {}".format(c, e, k)]
+
+                    have_search += 1
+                    print("=======================================================================================")
+                    print("score", score)
+                    print("c {} | epsilon {} | kernel {}".format(c, e, k))
+                    print("progress", have_search, "out of", total_search)
+                    print("=======================================================================================")
+
+    print("best score", best_score)
+    print("best search regressor", best_search[0])
+    print("best search detail", best_search[1])
 
 
 if __name__ == "__main__":
